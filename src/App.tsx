6fbf0500d8b87cad97da771b96dc22b92aa37f86
nader2356 <naderabdessaied8@gmail.com>
2023-10-28 13:35:28 +0200

    add required  auth method  in page app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,13 @@ import { Loader2 } from 'lucide-react'
 import { Suspense, lazy } from 'react'
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-
+import FullPageSpinner from './components/full-page-spinner'
 const LoginPage = lazy(() => import('./pages/login-page'))
 const RegisterPage = lazy(() => import('./pages/register-page'))
 const HomePage = lazy(() => import('./pages/home-page'))
 
-function FullPageSpinner() {
-	return (
-		<div className='flex justify-center items-center h-screen'>
-			<Loader2 className='animate-spin' size={64} />
-		</div>
-	)
-}
+
+
 function requireAuth(component: React.ReactNode) {
 	return <AuthProvider>{component}</AuthProvider>
 }
