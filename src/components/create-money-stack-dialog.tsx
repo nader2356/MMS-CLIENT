598b65b8cf82148b688af609137f7d05df1f0386
nader2356 <naderabdessaied8@gmail.com>
2023-10-29 16:58:11 +0100

    correct the import of cn into some file

--- src/components/create-money-stack-dialog.tsx
+++ src/components/create-money-stack-dialog.tsx
@@ -6,10 +6,11 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '../components/ui/dialog'
-import { cn } from '@/lib/utils'
+
 import { buttonVariants } from './ui/button'
 import { CreateMoneyStackForm } from './forms/create-money-stack'
 import { useState } from 'react'
+import { cn } from '../lib/utils'
 
 export default function CreateMoneyStackDialog() {
 	const [isShowDialog, setIsShowDialog] = useState(false)
